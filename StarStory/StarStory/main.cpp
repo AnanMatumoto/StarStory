8190c804c6b12777645b8588e9015c559a7453bc
AnanMatumoto <matumoto.anan@gmail.com>
2019-03-16 16:04:20 +0900

    テストコードの削除

--- StarStory/StarStory/main.cpp
+++ StarStory/StarStory/main.cpp
@@ -1,70 +1,22 @@
 ﻿#include "Lib.h"
-
-
-
-//　以下テスト用
-#define IMG_BACK "Resource/test_image/back_sample.jpg"
-#define IMG_PLYR "Resource/test_image/Player.jpg"
-
-void BackGraund() {//背景
-
-	Lib::DrawPx2D(
-		IMG_BACK,
-		0, 0,
-		1920,1080
-
-	);
-
-}
-
-class Player {
-
-public:
-	void PlayerUpdate() {//プレイヤー
-		
-		++flame;
-		
-		Lib::DrawTriangle2D(
-			IMG_PLYR,
-			vtx,
-			x, y
-		);
-
-		/*Lib::RotTriangle2D(
-			IMG_PLYR,
-			flame,
-			x, y,
-			128,128#
-			);*/
-	}
-	float x=0, y =0;
-	Vertex vtx[3];
-	int flame = 0;
-
-	Player() {}
-	~Player(){}
-};
+#include "SceneManager.h"
 
 
 // メイン
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	Lib::Init(1920, 1080, "StarStory");
-	Player p;
-	
+	SceneManager* sm = new SceneManager();
 
 
 	int c = 0;
 	while (Lib::ProcessMessage()) {
 
 		Lib::DrawBegin(0xe0e0ff);
-		Lib::GetMousePoint(&p.x, &p.y);
-
-
-		BackGraund();
-		p.PlayerUpdate();
-
-
+		
+		sm->Init();
+		sm->Update();
+		
 
 		Lib::DrawEnd();
 
