5212ac2e079cca2fc3dd3b50053bf8566a055b81
AnanMatumoto <matumoto.anan@gmail.com>
2019-03-17 00:15:33 +0900

    テストコードの削除

--- StarStory/StarStory/main.cpp
+++ StarStory/StarStory/main.cpp
@@ -7,17 +7,20 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	Lib::Init(1920, 1080, "StarStory");
 	SceneManager* sm = new SceneManager();
-
+	sm->Init();
 
 	int c = 0;
 	while (Lib::ProcessMessage()) {
 
+		Lib::KeyUpdate();
+		
+		//　描画開始
 		Lib::DrawBegin(0xe0e0ff);
 		
-		sm->Init();
+	
 		sm->Update();
 		
-
+		//　描画終了
 		Lib::DrawEnd();
 
 	}
