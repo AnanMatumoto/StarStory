dad40e0ebe50949a0152abec33e9a2880bf7b20a
AnanMatumoto <matumoto.anan@gmail.com>
2019-03-18 18:22:46 +0900

    シーンマネージャーの実装を一部修正

--- StarStory/StarStory/Scene/SceneManager.cpp
+++ StarStory/StarStory/Scene/SceneManager.cpp
@@ -1,17 +1,30 @@
 ﻿#include "SceneManager.h"
-#include "Scene.h"
+#include "TitleScene.h"
+#include "CustomScene.h"
+#include "SelectScene.h"
+#include "GameScene.h"
+#include "ResultScene.h"
 
-SceneManager::SceneManager() {
-
-	scene = new SceneBase;
+//---------------------------------
+//インスタンス取得
+SceneManager& SceneManager::GetInstance() {
+	static SceneManager sm;
+	return sm;
 }
 
 //--------------------------------
 //  シーン初期化
 void SceneManager::Init() {
 
-	Scene::g_StateID = SS_INIT;
-	Scene::g_SceneID = SC_TITLE;
+	//シーンの登録
+	SceneRegister(SC_TITLE,  new TitleScene());
+	SceneRegister(SC_CUSTOM, new CustomScene());
+	SceneRegister(SC_SELECT, new SelectScene());
+	SceneRegister(SC_GAME,   new GameScene());
+	SceneRegister(SC_RESULT, new ResultScene());
+
+	auto it = scene_list.find(SC_TITLE);
+	scene = it->second;
 }
 
 //---------------------------------
@@ -19,40 +32,61 @@ void SceneManager::Init() {
 void SceneManager::Update() {
 
 
-	switch (Scene::g_SceneID)
+	switch (scene_id)
 	{
 	case SC_TITLE:
-		scene = new TitleScene();
 		scene->Control();
 		break;
 
 	case SC_CUSTOM:
-		scene = new CustomScene();
 		scene->Control();
 		break;
 	
 	case SC_SELECT:
-		scene = new SelectScene();
 		scene->Control();
 		break;
 
 	case SC_GAME:
-		scene = new GameScene();
 		scene->Control();
 		break;
 
 	case SC_RESULT:
-		scene = new ResultScene();
 		scene->Control();
 		break;
 	}
+}
 
+//------------------------------------
+//　シーンの登録
+void SceneManager::SceneRegister(
+	SceneID id,
+	SceneBase* sb
+) {
+	scene_list.emplace(id, sb);
+}
+
+//-------------------------------------
+//　シーン遷移
+void SceneManager::CreateNextScene(SceneID scene_id) {
+	auto itr = scene_list.find(scene_id);
+	scene = itr->second;
+}
+
+//--------------------------------------
+//　シーンIDのセッター
+void SceneManager::SetSceneID(SceneID sc_id) {
+	scene_id = sc_id;
 }
 
 
-//--------------------------------
-//　ゲームオブジェクトの追加
-void SceneManager::AddGameObject() {
+//--------------------------------------
+//　デストラクタ
+SceneManager::~SceneManager() {
 
+	//リスト内の破棄
+	for (auto & it : scene_list) {
+		it.second->~SceneBase();
+	}
 }
+
 
--- StarStory/StarStory/Scene/SceneManager.h
+++ StarStory/StarStory/Scene/SceneManager.h
@@ -1,18 +1,36 @@
 ﻿#pragma once
+#include "Scene.h"
+#include <unordered_map>
 
-class SceneBase;
+/*
+	＜シーンマネージャークラス＞
+	Init　		   ：初期化処理
+	Update		   ：更新処理
+	CreateNextScene:シーン遷移処理
+	SetSceneID     ：シーンIDセット
+	SceneRegister  ：シーン登録処理
 
+*/
 class SceneManager {
 
 public:
 
-	SceneManager();
+	//シングルトン
+	static SceneManager& GetInstance();
 	void Init();
 	void Update();
-	void AddGameObject();
-	~SceneManager(){}
+	void CreateNextScene(SceneID);
+	void SetSceneID(SceneID sc_id);
+	//void AddGameObject();
+	~SceneManager();
+
 private:
+	void SceneRegister(
+		SceneID scene_id,
+		SceneBase* scene
+	);
+	
+	std::unordered_map<SceneID, SceneBase*> scene_list;
 	SceneBase* scene;
-
-
+	SceneID    scene_id;
 };
