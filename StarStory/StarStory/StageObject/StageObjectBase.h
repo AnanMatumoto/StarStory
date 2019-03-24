0bc937ffaf556eafba8b0ac90bd1e883d4ad5105
AnanMatumoto <matumoto.anan@gmail.com>
2019-03-24 12:06:53 +0900

    [env #1]ステージオブジェクト基底クラスを作成

---
+++ StarStory/StarStory/StageObject/StageObjectBase.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+#include "../Lib/Lib.h"
+
+//===================================
+//　ステージオブジェクト基底クラス
+//===================================
+
+/*
+	ステージに置かれるオブジェクトは
+	以下を継承する。
+
+	Update：更新処理
+	Draw　：描画処理
+
+*/
+
+class StageObjectBase {
+	
+public:
+
+	StageObjectBase(){}
+	virtual void Update(){}
+	virtual void Draw(){}
+	virtual ~StageObjectBase(){}
+
+protected:
+
+	t_Float2 pos; // 座標
+	float rot;    // 回転角度
+};
