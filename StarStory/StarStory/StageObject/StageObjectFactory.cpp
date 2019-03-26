5fb3774a6194baa60d4f95fe8cfb33aaa76fca30
AnanMatumoto <matumoto.anan@gmail.com>
2019-03-26 19:15:12 +0900

    Factoryを継承したStageObjectFactoryクラスを作成

---
+++ StarStory/StarStory/StageObject/StageObjectFactory.cpp
@@ -0,0 +1,28 @@
+﻿#include "StageObjectFactory.h"
+#include "ObjectTest1.h"
+
+//---------------------------------
+//　ステージオブジェクト生成処理
+ObjectBase* StageObjectFactory::Create(int id) {
+	switch (id)
+	{
+	case OBJ_TEST1:
+		return new ObjectTest1();
+		break;
+	}
+	return nullptr;
+}
+
+//-----------------------------------
+// クローン生成処理
+ObjectBase* StageObjectFactory::Clone(int id) {
+
+	switch (id)
+	{
+	case OBJ_TEST1:
+		return new ObjectTest1();
+		break;
+	}
+	return nullptr;
+}
+
---
+++ StarStory/StarStory/StageObject/StageObjectFactory.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+#include "Factory.h"
+
+enum StageObjectID {
+
+	OBJ_TEST1,
+};
+
+//===================================
+//　ステージオブジェクト生成クラス
+//===================================
+
+class StageObjectFactory:public Factory{
+
+public:
+	StageObjectFactory(){}
+	~StageObjectFactory(){}
+
+	// オブジェクト生成処理
+	ObjectBase* Create(int id)override;
+	// クローン生成処理
+	ObjectBase* Clone(int id)override;
+
+};
+
