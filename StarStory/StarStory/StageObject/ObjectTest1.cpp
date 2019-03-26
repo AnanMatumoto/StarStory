499a2f306a2740e16fd8737eb38575e85ca8e223
AnanMatumoto <matumoto.anan@gmail.com>
2019-03-26 19:14:07 +0900

    オブジェクト基底クラスを継承したテスト用派生クラスの追加

---
+++ StarStory/StarStory/StageObject/ObjectTest1.cpp
@@ -0,0 +1,36 @@
+﻿#include "ObjectTest1.h"
+#include "../Lib/Lib.h"
+
+//---------------------------------
+//　コンストラクタ
+ObjectTest1::ObjectTest1() {
+
+	pos = { 0,0 };
+	rot = 0;
+}
+
+//--------------------------------
+//　更新処理
+void ObjectTest1::Update() {
+
+
+}
+
+//---------------------------------
+//　描画処理
+void ObjectTest1::Draw() {
+
+	Lib::DrawPx2D(
+		"HogeHoge",
+		pos.m_x, pos.m_y,
+		100, 100
+	);
+}
+
+//---------------------------------
+//　解放処理
+void ObjectTest1::Delete() {
+
+	delete this;
+}
+
---
+++ StarStory/StarStory/StageObject/ObjectTest1.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+#include "ObjectBase.h"
+
+//================================
+// テスト用オブジェクトクラス	
+//================================
+
+class ObjectTest1 :public ObjectBase {
+
+public:
+	ObjectTest1();
+
+private:
+
+	//　更新処理
+	void Update()override;
+	//　描画処理
+	void Draw()override;
+	//　解放処理
+	void Delete()override;
+	~ObjectTest1(){}
+
+};
+
