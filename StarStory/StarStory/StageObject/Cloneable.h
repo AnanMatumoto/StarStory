91d80d01aee24622a8db778e43798f5f13b3eaf7
AnanMatumoto <matumoto.anan@gmail.com>
2019-03-29 23:36:53 +0900

    複製インターフェースを追加

---
+++ StarStory/StarStory/StageObject/Cloneable.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+
+//===================================
+//  複製基底クラス
+//===================================
+
+/*
+	Cloneableクラスを継承した場合、
+	以下の関数により自身を複製する。
+
+	Clone:　複製関数
+*/
+
+class Cloneable {
+
+public:
+
+	Cloneable(){}
+	virtual Cloneable* Clone(){
+	
+		return new Cloneable();
+	}	
+	virtual ~Cloneable(){}
+};
