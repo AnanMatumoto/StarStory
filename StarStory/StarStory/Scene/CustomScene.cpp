4c9c7366d14d9e8cee27729b9cdae5b66f43e2e3
ryusei1203 <ryusei.jp1203@gmail.com>
2019-04-15 17:01:49 +0900

    考えやすくするために、仮画像だけ描画しました

--- StarStory/StarStory/Scene/CustomScene.cpp
+++ StarStory/StarStory/Scene/CustomScene.cpp
@@ -52,8 +52,61 @@ SceneID CustomScene::Control() {
 //　カスタムシーン描画
 void CustomScene::Draw() {
 	
+	// 描画順に書いていく
+	// 橋本君のラフを元に置いてます(全て仮画像)
+
+	// 「背景」
 	Lib::DrawBox2D(
 		"Resource/test_image/samp_custom.png",
 		0, 0
 	);
+
+	// 「星の後ろの白枠」
+	Lib::DrawBox2D(
+		"Resource/Custom/星の後ろの白枠.png",
+		100, 100
+	);
+
+	// 「ひし形」
+	Lib::DrawBox2D(
+		"Resource/Custom/player_1_normal.png",
+		500, 500
+	);
+
+	// 「カスタム」
+	Lib::DrawBox2D(
+		"Resource/Custom/カスタム.png",
+		50, 50
+	);
+
+	// 「総スキル」
+	Lib::DrawBox2D(
+		"Resource/Custom/総スキル.png",
+		1200, 300
+	);
+
+	// 「スキル各種」
+	Lib::DrawBox2D(
+		"Resource/Custom/スキル各種.png",
+		1200, 500
+	);
+
+	// 「マップ確認」
+	Lib::DrawBox2D(
+		"Resource/Custom/マップ確認.png",
+		1200, 700
+	);
+
+	// 「セレクトへ」
+	Lib::DrawBox2D(
+		"Resource/Custom/セレクトへ.png",
+		1200, 800
+	);
+
+	//「プレイ画面へ」
+	Lib::DrawBox2D(
+		"Resource/Custom/プレイ画面へ.png",
+		1600, 700
+	);
 }
+
