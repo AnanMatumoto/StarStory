fb60919ec8bf86310987c1e599dfc708f99b0737
ryusei1203 <ryusei.jp1203@gmail.com>
2019-04-16 14:37:21 +0900

    パス指定間違っていたので変更

--- StarStory/StarStory/Custom/CustomStar/CustomStar.h
+++ StarStory/StarStory/Custom/CustomStar/CustomStar.h
@@ -1,7 +1,7 @@
 ﻿#pragma once
 
-#include"../Lib/Lib.h"
-#include"../Collision/Collision.h"
+#include"../../Lib/Lib.h"
+#include"../../Collision/Collision.h"
 #include"../DiamondBase/DiamondBase.h"
 
 class CustomStar: public DiamondBase{
