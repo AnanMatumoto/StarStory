e68e751a3eb31166497faf694896ece93dd4e521
ryusei1203 <ryusei.jp1203@gmail.com>
2019-04-16 14:22:31 +0900

    カスタムフォルダの中にカスタムスター.cpp.hを作って入れた。ダイアモンドベースを入れた。

---
+++ StarStory/StarStory/Custom/CustomStar/CustomStar.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+
+#include"../Lib/Lib.h"
+#include"../Collision/Collision.h"
+#include"../DiamondBase/DiamondBase.h"
+
+class CustomStar: public DiamondBase{
+
+public:
+
+private:
+
+};
