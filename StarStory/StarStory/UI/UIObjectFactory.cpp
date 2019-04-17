6a65f9cc8d7246c21e9c879893d63f958b2eb202
AnanMatumoto <matumoto.anan@gmail.com>
2019-04-17 10:36:20 +0900

    UIクラスの一部をコメントアウト

--- StarStory/StarStory/UI/UIObjectFactory.cpp
+++ StarStory/StarStory/UI/UIObjectFactory.cpp
@@ -1,20 +1,21 @@
 ﻿#include "UIObjectFactory.h"
 #include "ButtonUI.h"
 
-ObjectBase* UIObjectFactory::Create(
-	int id,
-	float x, float y,
-	float rotate
-) {
-
-	switch (id)
-	{
-	case BT_START:
-		new ButtonUI(x,y);
-		break;
-	case BT_END:
-		new ButtonUI(x, y);
-		break;
-	}
-	return nullptr;
-}
+//
+//ObjectBase* UIObjectFactory::Create(
+//	int id,
+//	float x, float y,
+//	float rotate
+//) {
+//
+//	switch (id)
+//	{
+//	case BT_START:
+//		new ButtonUI(x,y);
+//		break;
+//	case BT_END:
+//		new ButtonUI(x, y);
+//		break;
+//	}
+//	return nullptr;
+//}
