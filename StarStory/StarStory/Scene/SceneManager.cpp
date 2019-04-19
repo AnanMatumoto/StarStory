cf641e8d90771254f50fbf7acae185204e22e742
ryusei1203 <ryusei.jp1203@gmail.com>
2019-04-19 09:41:13 +0900

    シーンをタイトルからに変更

--- StarStory/StarStory/Scene/SceneManager.cpp
+++ StarStory/StarStory/Scene/SceneManager.cpp
@@ -24,7 +24,7 @@ void SceneManager::Init() {
 	scene_list.emplace(SC_RESULT, new ResultScene());
 
 	//初期設定でタイトルをセットする
-	auto it = scene_list.find(SC_CUSTOM);
+	auto it = scene_list.find(SC_TITLE);
 	m_scene = it->second;
 	m_now_id = SC_TITLE;
 }
