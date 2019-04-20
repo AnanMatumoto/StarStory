336e190dcbb4a09d8137173b28f9f0f1e83a87f3
AnanMatumoto <matumoto.anan@gmail.com>
2019-04-20 12:36:33 +0900

    UIマネージャークラスを追加

---
+++ StarStory/StarStory/UI/UIManager.h
@@ -0,0 +1,76 @@
+﻿#pragma once
+
+#include "../StageObject/ObjectBase.h"
+#include <unordered_map>
+#include <string>
+
+enum UIObjectID {
+
+	//------UI---------
+	//タイトルボタン
+	BT_TT_START,		//タイトル画面スタート
+	BT_TT_END,		//  　〃　　　終了
+
+	//カスタム
+	BT_CT_DEC,
+	BT_CT_MAP,
+	BT_CT_TOSELECT,
+
+	//ゲームボタン
+	BT_GM_WIND,
+	BT_GM_TOCUSTOM,
+	BT_GM_STOP,
+	BT_GM_ONOFF,
+
+	//リザルト
+	BT_RS_SELECT,		//ステージセレクトへ
+	BT_RS_CUSUTOM,		//カスタムへ
+
+};
+
+
+
+//=================================
+// UI管理クラス
+//=================================
+
+class UIManager {
+
+public:
+	// インスタンス生成処理
+	static UIManager& GetInstance();
+
+	/*
+	   UIの登録
+
+	   第1：UIのID
+	   第2：X座標
+	   第3：Y座標
+	   第4：画像名
+	
+	*/
+	void Register(
+		UIObjectID id,
+		float x, float y,
+		std::string tex_name
+	);
+
+	//更新処理
+	void Update();
+
+	//  描画処理
+	void Draw();
+
+	//  IDで指定したオブジェクトを削除する
+	void Delete(UIObjectID id);
+
+	//　リスト内にあるオブジェクトを全て削除する
+	void AllDelete();
+
+	//　引数でもらったidのオブジェクトを取得する
+	ObjectBase* FindObject(UIObjectID id);
+
+private:
+
+	std::unordered_map<UIObjectID, ObjectBase*>m_UI_list;//UIリスト
+};
