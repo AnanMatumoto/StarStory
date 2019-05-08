39ece464e12e341327d4e9ab7fe9ba0cb0922d72
AnanMatumoto <matumoto.anan@gmail.com>
2019-05-08 18:18:06 +0900

    めり込んだ際のy座標の修正処理を追加

--- StarStory/StarStory/StageObject/StarObject.cpp
+++ StarStory/StarStory/StageObject/StarObject.cpp
@@ -29,11 +29,12 @@ StarObject::StarObject(
 	m_width  = 128;
 	m_height = 128;
 	m_vel.x  = 0;
-	m_jump_power    = -4.f;
-	m_interval = 120.f;
-	m_is_active = false;
-	m_cur_child = nullptr;
-	m_cur_obj   = nullptr;
+	m_jump_power = -2.5f;
+	m_interval   = 60.f;
+	m_is_active  = false;
+	m_cur_child  = nullptr;
+	m_cur_obj    = nullptr;
+	m_inset_y    = 0.f;
 }
 
 //-----------------------------------
@@ -46,15 +47,18 @@ void StarObject::Update() {
 		m_childs = mng.GetGameObjects<StarChild>();
 	}
 
-	//自動操縦
-	AutomaticMove();
-
 	if (m_cur_child != nullptr) {
 		//子供からもらったスキルを発動する
 		SkillActive(m_cur_child->GetSkill());
 	}
 
+	//自動操縦
+	AutomaticMove();
+
+
+
 	SceneBase* gc = SceneManager::GetInstance().GetScene();
+	//AdjustPosY();
 
 	float out_w = m_pos.x + m_width;
 	float out_h = m_pos.y + m_height;
@@ -89,15 +93,16 @@ void StarObject::SetVertex(DWORD color) {
 // 自動操作
 void StarObject::AutomaticMove() {
 
+	float hit_obj_w = 0;
+	float hit_obj_h = 0;
+	ObjectBase* obj = nullptr;
+	float dis = 0.f;
+
 	if (!m_is_active) {
 		//スキルが発動していない場合
 		AddMoveAmount(m_speed, GRAVITY);
 		RefPosition();
 	}
-
-	float hit_obj_w = 0;
-	float hit_obj_h = 0;
-	ObjectBase* obj = nullptr;
 
 	for (auto child : m_childs) {
 
@@ -108,37 +113,38 @@ void StarObject::AutomaticMove() {
 			obj = m_cur_child->GetMapObj();
 			hit_obj_w = obj->GetVertex(1).pos.x;
 			hit_obj_h = obj->GetVertex(0).pos.y;
-
+			
+			m_pos.y += m_cur_child->DistanceToCeiling()+0.5;
 			m_is_active = true;
 		}
 	}
-
 	//オブジェクトの幅を超えた、またはオブジェクトより下にいる場合
-	if (m_pos.x >= hit_obj_w||m_pos.y >= hit_obj_h) {
+	if (m_pos.x > hit_obj_w||m_pos.y > hit_obj_h) {
 		m_is_active = false;
 	}
-	
 }
 
 //------------------------------------
 
 void StarObject::SkillActive(int skill_id) {
-
+	
 	switch (skill_id)
 	{
 	case NORMAL://スキルセットなし
 		AddMoveAmount(m_speed);
 		RefPosition();
-		m_interval = 120.f;
+		m_interval = 60.f;
 		break;
 
 	case SPEED://加速スキル
 		AddMoveAmount(m_speed* BOOST);
 		RefPosition();
+	
 		break;
 
 	case JUMP://ジャンプスキル
 		JumpMotion();
+		m_is_active = false;
 		break;
 	case STOP://停止スキル
 		StopMotion();
@@ -167,6 +173,7 @@ void StarObject::RefPosition() {
 
 	m_pos.x += m_vel.x;
 	m_pos.y += m_vel.y;
+	m_is_active = false;
 }
 
 //--------------------------------
@@ -176,11 +183,7 @@ void StarObject::JumpMotion(){
 
 	if (--m_interval > 0) {
 		//ジャンプ時間内である
-		AddMoveAmount(m_speed, m_jump_power,0.f);
-		RefPosition();
-	}
-	else {
-		AddMoveAmount(0.f, GRAVITY, 0.f);
+		AddMoveAmount(0, m_jump_power,0.f);
 		RefPosition();
 	}
 }
@@ -191,8 +194,18 @@ void StarObject::StopMotion() {
 		AddMoveAmount(0.f, 0.f, 0.f);
 		RefPosition();
 	}
-	else {
-		AddMoveAmount(m_speed);
-		RefPosition();
+	m_is_active = false;
+}
+
+void StarObject::AdjustPosY() {
+
+	float dis = m_pos.y + m_inset_y;
+
+	if (m_is_active) {
+		if (dis >m_inset_y) {
+			m_pos.y += 1.f;
+		}
+
 	}
+
 }
--- StarStory/StarStory/StageObject/StarObject.h
+++ StarStory/StarStory/StageObject/StarObject.h
@@ -41,13 +41,16 @@ private:
 	// 停止モーション
 	void StopMotion();
 
+	void AdjustPosY();
+
 private:
 
 	Vec2  m_vel;						//移動量
 	Vec2  m_temp_pos;					//保存用移動量
 	float m_speed;						//速さ
 	float m_jump_power;					//ジャンプ力
-	float m_interval;				//ジャンプ時間
+	float m_interval;				    //ジャンプ時間
+	float m_inset_y;					//めり込みY座標
 	bool  m_is_active;					//スキル発動中
 	StarChild* m_cur_child;
 	MapObject* m_cur_obj;
