79704a14b30b841e5e151d09e0eb2dfb42698f81
ryusei1203 <ryusei.jp1203@gmail.com>
2019-05-28 16:26:12 +0900

    Skill

---
+++ StarStory/StarStory/ReviewSkill.h
@@ -0,0 +1,48 @@
+﻿#pragma once
+
+// 各スキルの識別群
+enum Skill {
+
+	NORMAL,
+	SPEED,
+	JUMP,
+	STOP,
+	LIGHT
+};
+
+// スキルIDクラス
+// シングルトン
+class Skill_ID {
+
+public:
+
+	// インスタンス取得
+	static Skill_ID &GetInstance() {
+
+		static Skill_ID skill_id;
+		return skill_id;
+	}
+
+	// セッター
+	void SetSkillID(Skill skill) {
+
+		m_skill = skill;
+	}
+
+	// ゲッター
+	Skill GetSkillID() {
+
+		return m_skill;
+	}
+
+private:
+
+	// スキルID保管用
+	Skill m_skill;
+
+	// コンストラクタ
+	Skill_ID() : m_skill(NORMAL) {};
+
+};
+
+
